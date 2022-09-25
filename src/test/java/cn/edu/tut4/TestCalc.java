604b633444d0a2998815f987407d5f17c9843b1a
Byrnn <3014761017@qq.com>
2022-09-25 21:59:33 +0800

    Third commit

--- src/test/java/cn/edu/tut4/TestCalc.java
+++ src/test/java/cn/edu/tut4/TestCalc.java
@@ -7,22 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Unit test for simple App.
  */
-public class TestCalc
-{
+public class TestCalc {
    @Test
-    public void TestAdd(){
-       Calc calc = new Calc();
-       int answer = calc.add(3,4);
-       assertEquals(answer,7);
+   public void TestAdd() {
+      Calc calc = new Calc();
+      int answer = calc.add(3, 4);
+      assertEquals(answer, 7);
    }
-<<<<<<< HEAD
-=======
-//   @Test
-//    public void TestSubstract(){
-//       Calc calc = new Calc();
-//       int answer = calc.subtract(5,1);
-//       assertEquals(answer,4);
-//   }
-
->>>>>>> a6e408b (Third commit)
 }
+
